1585172effa2ba5989a8c197b1a053cd56a05f81
mfglr <mfgglr@outlook.com>
2024-01-02 20:28:39 +0300

    refactor

--- app/main.cpp
+++ app/main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char const *argv[])
 
     //calculate the shortest path between index-15 and index-24 of the graph by ucs algorithm.
     Algorithm *ucs = new UCS(graph,15,24);
-    list<int> path1 = astar->run();
+    list<int> path1 = ucs->run();
 
     printf("%s\n","***************************");
     for(list<int>::iterator it = path0.begin(); it != path0.end();it++)
